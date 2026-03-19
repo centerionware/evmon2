8296e553b024e268fc6c6192011edca180564a4c
0xdeadc0de <0xdeadc0de@noreply.git.centerionware.com>
2026-03-19 14:52:32 +0000

    Update cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,21 @@ import (
 	_ "modernc.org/sqlite"
 	_ "github.com/lib/pq"
 	_ "github.com/go-sql-driver/mysql"
+
 	"centerionware.com/evmon/internal"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	networkingv1 "k8s.io/api/networking/v1"
+	"k8s.io/client-go/dynamic/dynamicinformer"
 )
 
 func main() {
@@ -61,34 +75,100 @@ func main() {
 		log.Fatalf("failed to create controller: %v", err)
 	}
 
+	// Kubernetes config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatalf("failed to get cluster config: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("failed to create clientset: %v", err)
+	}
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("failed to create dynamic client: %v", err)
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if err := controller.SyncIngresses(ctx); err != nil {
-		log.Printf("warning: failed to sync ingresses: %v", err)
-	}
-	if err := controller.SyncCRDs(ctx); err != nil {
-		log.Printf("warning: failed to sync CRDs: %v", err)
-	}
+	// --- Ingress Informer ---
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+	ingInformer := factory.Networking().V1().Ingresses().Informer()
 
-	go func() {
-		ticker := time.NewTicker(30 * time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-ticker.C:
-				if err := controller.SyncIngresses(ctx); err != nil {
-					log.Printf("warning: failed to sync ingresses: %v", err)
-				}
-				if err := controller.SyncCRDs(ctx); err != nil {
-					log.Printf("warning: failed to sync CRDs: %v", err)
+	ingInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			ing := obj.(*networkingv1.Ingress)
+			endpoints := controller.ExtractIngress(ing)
+
+			for _, ep := range endpoints {
+				if err := store.UpsertEndpoint(ep); err != nil {
+					log.Printf("failed to upsert ingress endpoint: %v", err)
 				}
 			}
-		}
-	}()
+		},
+		DeleteFunc: func(obj interface{}) {
+			ing := obj.(*networkingv1.Ingress)
+			serviceIDs := controller.ExtractIngressServiceIDs(ing)
 
+			for _, id := range serviceIDs {
+				if err := store.DeleteServiceID(id); err != nil {
+					log.Printf("failed to delete ingress serviceID: %v", err)
+				}
+			}
+		},
+	})
+
+	// --- CRD Informer ---
+	evmonGVR := schema.GroupVersionResource{
+		Group:    "evmon.centerionware.com",
+		Version:  "v1",
+		Resource: "evmonendpoints",
+	}
+
+	crdInformer := dynamicinformer.NewFilteredDynamicInformer(
+		dynClient,
+		evmonGVR,
+		metav1.NamespaceAll,
+		0,
+		cache.Indexers{},
+		nil,
+	)
+
+	crdInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			u := obj.(*unstructured.Unstructured)
+
+			ep := controller.ExtractCRD(u)
+
+			if err := store.UpsertEndpoint(ep); err != nil {
+				log.Printf("failed to upsert CRD: %v", err)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			u := obj.(*unstructured.Unstructured)
+
+			serviceID, _, _ := unstructured.NestedString(u.Object, "spec", "serviceID")
+
+			if err := store.DeleteServiceID(serviceID); err != nil {
+				log.Printf("failed to delete CRD serviceID: %v", err)
+			}
+		},
+	})
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	go ingInformer.Run(stopCh)
+	go crdInformer.Run(stopCh)
+
+	if !cache.WaitForCacheSync(stopCh, ingInformer.HasSynced, crdInformer.HasSynced) {
+		log.Fatal("failed to sync caches")
+	}
+
+	// Prober + API
 	prober := internal.NewProber(store, controller)
 	prober.Start()
 	defer prober.Stop()
@@ -107,8 +187,8 @@ func main() {
 		Handler: mux,
 	}
 
-	stopCh := make(chan os.Signal, 1)
-	signal.Notify(stopCh, syscall.SIGINT, syscall.SIGTERM)
+	stopSig := make(chan os.Signal, 1)
+	signal.Notify(stopSig, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		log.Printf("HTTP API listening on %s", server.Addr)
@@ -117,10 +197,14 @@ func main() {
 		}
 	}()
 
-	<-stopCh
+	<-stopSig
 	log.Println("shutting down Evmon...")
+
+	cancel()
+
 	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer shutdownCancel()
+
 	if err := server.Shutdown(shutdownCtx); err != nil {
 		log.Printf("HTTP server shutdown error: %v", err)
 	}
