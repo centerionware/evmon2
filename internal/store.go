34c58b9e1ad5541c925a8d75df0ea479058af484
0xdeadc0de <0xdeadc0de@noreply.git.centerionware.com>
2026-03-20 02:23:58 +0000

    Update internal/store.go

--- internal/store.go
+++ internal/store.go
@@ -13,17 +13,18 @@ import (
 type DBStore struct {
 	db     *sql.DB
 	dbType string
-    clientHook ClientHook
+    clientHook *ClientHook
 }
 
 func NewDBStore(db *sql.DB, dbType string) *DBStore {
 	return &DBStore{
 		db:     db,
 		dbType: dbType,
+        clientHook: nil,
 	}
 }
 
-func (s *DBStore) SetClientHook(hook ClientHook) {
+func (s *DBStore) SetClientHook(hook *ClientHook) {
 	s.clientHook = hook
 }
 
